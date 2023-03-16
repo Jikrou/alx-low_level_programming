5587189e269d45fc13970da4032fe06183e52fdf
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-16 16:49:32 -0700

    SizeOf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints the size of various types of data
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char a;
+int z;
+long e;
+long long int r;
+float t;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of an int: %zu byte(s)\n", sizeof(z));
+printf("Size of a long: %zu byte(s)\n", sizeof(e));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(r));
+printf("Size of a float: %zu byte(s)\n", sizeof(t));
+return (0);
+}
