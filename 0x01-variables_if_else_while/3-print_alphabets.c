5add19ae81c3a4a98f21a7c6aa7f0cc37f22ab41
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-17 16:33:38 -0700

    alPhaBet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints lowercase and uppercase letters
+ * of alphabet
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+putchar(i);
+for (int j = 'A'; j <= 'Z'; j++)
+putchar(j)
+putchar('\n');
+return (0);
+}
