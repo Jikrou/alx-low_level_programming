7e7e60ccee55b8a8b1e6c397b137ec9b980cbfd5
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-16 18:55:35 -0700

    Print without using a printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  *
  * Description: C program prints without using printf or puts
  *
- * return: 1
+ * return: 1 (not success)
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19"));
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19"));
 return (1);
 }
