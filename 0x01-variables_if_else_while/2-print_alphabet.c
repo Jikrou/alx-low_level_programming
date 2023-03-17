38c890f84708708d2743a4ced203d9fd625e2021
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-17 16:18:49 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program prints the alphabet in
+ * lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int ABC;
+
+for (ABC = 'a'; ABC <= 'z'; ABC++)
+putchar(ABC);
+putchar('\n');
+return (0);
+}
