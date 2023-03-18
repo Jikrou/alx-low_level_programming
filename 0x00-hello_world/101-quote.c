7ef8929e020a85f24d29399c2ca71aa95f7529df
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-18 16:43:24 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 */
 int main(void)
 {
-char qot[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
+char qot[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, qot, sizeof(qot) - 1);
 return (1);
 }
