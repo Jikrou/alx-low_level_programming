f4ef38bec0ae04681ff7dd7ae87a7a462af96ab2
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-17 17:45:06 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: C program that prints numbers of
+* base 10 through ASCII
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+for (i = 48; i <= 57; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
