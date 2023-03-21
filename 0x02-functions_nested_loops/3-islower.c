ae5c8fe0f0041cd506c523b2ac8f3d7aa3e91570
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 12:31:19 -0700

    Check if islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - a function that checks if c is lowercase
+ * @c: character to check
+ *
+ * Return: 1 if c is lowercase and 0 otherwise
+*/
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97)
+	{
+		return (1);
+	}
+	else if (c < 97)
+	{
+		return (0);
+	}
+	return (0);
+}
