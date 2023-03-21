ff08702d69556b32c86953de90e30e3f1afa2bad
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 15:33:53 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,28 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
+	int k;
+	int l;
 
-	for (i = 0; i < 24; i++)
-	{
-		for (j = 0; j < 59; j++)
-		{
-			_putchar(i + '0');
-			_putchar(':');
-			_putchar(j + '0');
-			_putchar('\n');
-		}
-	}
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (l = 0; l < 10; l++)
+{
+if (i != 2 && j != 4)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
 }
-
+}
+}
+}
+}
+}
