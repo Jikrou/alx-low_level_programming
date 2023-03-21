ed7b87eb32b6a73cfc992fb3b824ecaffee1eadc
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 13:40:21 -0700

    The absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - give the absolute value
+ * @int: the number to compute the absolute value
+ * Return: 0 (success)
+*/
+int _abs(int)
+{
+	int r;
+
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+	else
+	{
+		return (r);
+	}
+	return (0);
+}
