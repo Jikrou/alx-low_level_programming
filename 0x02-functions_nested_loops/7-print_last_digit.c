fa0e1e988d0b98fc704f6f08b7ab9ae3d59ce562
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 14:24:02 -0700

    the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a number
+ * print_last_digit - prints the last
+ * digit of a number
  * @x: the number to print of the last digit
  * Return: last digit
 */
 int print_last_digit(int x)
 {
-	return (x % 10);
+	int n;
+
+	n = x % 10;
+	if (n < 0)
+	{
+		_putchar(-n + 0);
+			return (-n);
+	}
+	else
+	{
+		_putchar(n + 0);
+		return (n);
+	}
 }
 
