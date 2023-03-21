428fd7f42fcd699a707dbc613f3045e3e2f0c923
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 15:43:23 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,15 +18,14 @@ for (k = 0; k < 6; k++)
 {
 for (l = 0; l < 10; l++)
 {
-if (i != 2 && j != 4)
-{
+if (i == 2 && j == 4)
+break;
 _putchar(i + '0');
 _putchar(j + '0');
 _putchar(':');
 _putchar(k + '0');
 _putchar(l + '0');
 _putchar('\n');
-}
 }
 }
 }
