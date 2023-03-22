63d97fa96053f6f31d29b777321c6e540098543f
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 18:13:22 -0700

    Multiplication

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,37 @@
 #include "main.h"
-/***/
+/**
+ * times_tables - print the multiplication tables
+ *
+ * Description:a function that take no arguement
+ * and prints the product of the multiplication tables
+ * starting from 0 to 9, separated by commas and space
+ * where i represent the columns and j the row
+ *
+ * Return:no value is returned
+*/
 void times_table(void)
 {
+int i, j;
 
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		int multi = i * j;
+
+		_putchar(multi + '0');
+
+		if (j == 9)
+		{
+			_putchar('\n');
+		}
+		else if (j < 9)
+		{
+			_putchar(',  ');
+		}
+		else
+		{
+			_putchar(', ');
+		}
+	}
+}
