cd75f430e5f228317d9cd064bb22f490a43501b7
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 17:05:28 -0700

    addition,

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - addition function
+ * @i: the int to add
+ * @j: the int to add
+ *
+ * Return: the total of the  two integer
+*/
+int add(int i, int j)
+{
+	int i;
+	int j;
+	int total;
+
+	total = i + j;
+	return (total);
+}
