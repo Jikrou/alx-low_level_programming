35d438f92421a55844bec204749bb08a73515340
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-22 15:43:54 -0700

    9 times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,30 +14,25 @@ for (j = 0; j <= 9; j++)
 {
 int multi = i * j;
 
-if (multi == 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar('0');
-_putchar(',');
-}
-else if (multi < 10)
+if (multi < 10)
 {
 _putchar(' ');
 _putchar(' ');
 _putchar((multi % 10) + '0');
-_putchar(',');
 }
 else
 {
 _putchar(' ');
 _putchar((multi / 10) + '0');
 _putchar((multi % 10) + '0');
-_putchar(',');
 }
 if (j == 9)
 {
 _putchar('\n');
+}
+else 
+{
+	_putchar(',');
 }
 }
 }
