c379fed34971ba73ca90866b0656cebc2aaf624b
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-21 19:35:04 -0700

    multi tables 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,13 +25,11 @@ _putchar('0');
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(' ');
 }
 else if (multi < 10)
 {
-_putchar(multi + '0');
+_putchar((multi % 10) + '0');
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 }
