bdb0159f6ea20189db0c235eef0c3a1a83a1e0a6
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-23 16:22:28 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,20 +7,23 @@
 */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	i = 0;
-	if (n > 0)
-	{
-		while (i < n)
-		{
-			_putchar('\');
-			i++;
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
