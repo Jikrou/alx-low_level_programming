296ac8cd066f864d051547acb509fc8b00b4ae0a
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-22 18:33:08 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_times_table - print the times table of the input n
+ *  starting with 0.
+ * @n: the number of times table
+ * Return: Empty output
+*/
+void print_times_table(int n)
+{
+int i, j, multi;
+
+for (i = 0; i <= n; i++)
+{
+_putchar('0');
+_putchar(',');
+for (j = 1; j <= n; j++)
+{
+multi = i * j;
+if (multi < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(multi + '0');
+}
+else if (multi <= 99 && multi >= 10)
+{
+_putchar(' ');
+_putchar((multi / 10) + '0');
+_putchar((multi % 10) + '0');
+}
+else if (multi >= 100)
+{
+_putchar((multi / 100) + '0');
+_putchar(((multi / 10)) % 10 + '0');
+_putchar((multi % 10) + '0');
+}
+if (j == n)
+_putchar('\n');
+else
+_putchar(',');
+
+}
+}
+}
