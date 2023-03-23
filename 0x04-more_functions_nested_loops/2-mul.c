2af7f5421aececcce8e630180a7c55ac1badf8bc
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-23 10:45:01 -0700

    multy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integer.
+ * @a: the first character
+ * @b: the second character.
+ *
+ * Return: 0 (exit code).
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
