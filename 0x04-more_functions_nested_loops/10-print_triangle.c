b402551948a0bc08f95ca97010d8776237febe21
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-23 19:29:31 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: the size of the triangle
+ * Return: no value
+*/
+void print_triangle(int size)
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i + j < size - 1)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
