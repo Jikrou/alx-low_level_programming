bec2e3a9115d5a18490a709bf8b8a42978a1145f
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-24 18:16:11 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that finds and prints the largest
+ *  prime factor of the number 612852475143,
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int long n = 612852475143;
+	int i = 2;
+
+	while (n != 1)
+	{
+		if (n % i == 0)
+		{
+			while (n % i == 0)
+			{
+				printf("%d ", i);
+			       n = n / i;
+			}
+			i++;
+		}
+	}
+printf("\n");
+return (0);
+}
