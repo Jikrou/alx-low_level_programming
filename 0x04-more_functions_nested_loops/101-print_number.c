37e95f9efcd450d51015df931592221906562d12
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-24 20:12:18 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,15 @@
 */
 void print_number(int n)
 {
-if (n < 0)
+if (n <= 0 || n > 0)
 {
 _putchar(n + '0');
 }
-else if (n <= 10)
+else if (n > 10 && n < 100)
 {
 _putchar((n % 10) + '0');
 }
-else if (n <= 100)
+else if (n >= 100)
 {
 _putchar((n / 100) + '0');
 _putchar((n / 10) % 10 + '0');
