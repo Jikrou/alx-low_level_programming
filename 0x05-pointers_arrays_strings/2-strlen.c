b38c873a3bfb14553906e6aeed688ded093ea1b7
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-27 16:09:49 -0700

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string
+ * Return: integer the length
+*/
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	return (len + 1);
+}
