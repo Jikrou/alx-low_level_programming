5a91d0bfb19f425f4317b6a5be1af0386e3e8f93
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-29 14:10:18 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: a pointer to dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*i != '\0')
+	{
+		i++;
+	}
+	while (*src != '\0')
+	{
+		*i = *src;
+		i++;
+		scr++;
+	}
+	*i != '\0';
+	return (dest);
+}
