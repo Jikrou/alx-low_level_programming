3d8ef7d3a8837ccb7ed6ea518386b31091a15742
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-30 16:39:00 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - a fuction that changes all the
+ * lowercase letters of a string to uppercase
+ * @st: string
+ * Return: a character
+*/
+char *string_toupper(char *st)
+{
+	while (*st != '\0')
+	{
+		if (*st >= 'a' && *st <= 'z')
+		{
+			*st = *st - 32;
+		}
+		st++;
+	}
+	return (st);
+}
+
