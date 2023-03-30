1426d1e10e4f1f739088ea0a5d8d082a88a3cc82
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-30 14:46:15 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -25,5 +25,13 @@ int _strcmp(char *s1, char *s2)
 		}
 
 	}
+	if (s2[i] == '\0' && s1[i] != '\0')
+	{
+		return (15);
+	}
+	else if (s1[i] == '\0' && s2[i] != '\0')
+	{
+		return (-15);
+	}
 	return (0);
 }
