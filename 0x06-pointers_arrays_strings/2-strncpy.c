440948b2a799b8528b8ed19f1b562e1afbce3272
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-29 17:19:46 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: number of bytes
+ *Return: a pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *i = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*i = *src;
+		i++;
+		src++;
+		n--;
+	}
+	if (n < *src)
+	{
+		while (*i != '\0')
+			i++;
+	}
+	*i = '\0';
+	return (dest);
+}
