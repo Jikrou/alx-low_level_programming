2f11c445f5344a4b0559146b2e4d8af7b0667771
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-03-29 20:50:54 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,19 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	int i = 0;
+
+	while (s1[i] != '\0')
 	{
-		if (*s1 > *s2)
+		if (s1[i] == s2[i])
+		{
+			i++;
+		}
+		else if (s1[] > s2[i])
 		{
 			return (15);
 		}
-		else if (*s1 < *s2)
+		else if (s1[i] < s2[i])
 		{
 			return (-15);
 		}
