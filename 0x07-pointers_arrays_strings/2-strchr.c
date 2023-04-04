54c5a6b0640c40cb8d525c54c66833d1c1e77503
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-03 17:17:21 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a characterin a string
+ * @s: string
+ * @c: character
+ * Return: a pointer to the first occurrence
+*/
+char *_strchr(char *s, char c)
+{
+	char *st = s;
+
+	while (*st != '\0')
+	{
+		st++;
+		if (*st == c)
+		{
+			return (st);
+		}
+	}
+	return (NULL);
+}
