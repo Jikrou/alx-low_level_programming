cf7aaafe1f081a09ec1fb0e7d08b009a37bf9f97
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-04 17:13:57 -0700

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,26 +3,28 @@
 /**
  * print_diagsums - a function that prints the sum of the two
  * diagonals of a square matrix of integers.
- * @a:  */
+ * @a: a pointer to the first element of a square matrix of integers
+ * @size: size of the matrix
+*/
 void print_diagsums(int *a, int size)
 {
 	int i, j;
 	int sum0 = 0, sum1 = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				sum0 += *(a + i * size +j);
-			}
-			if (i == size - j - 1)
-			{
-				sum1 += *(a + i * size + j);
-			}
-		}
-	}
-	printf("%d, ", sum0);
-	printf("%d\n", sum1);
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+{
+sum0 += *(a + i * size + j);
+}
+if (i == size - j - 1)
+{
+sum1 += *(a + i * size + j);
+}
+}
+}
+printf("%d, ", sum0);
+printf("%d\n", sum1);
 }
