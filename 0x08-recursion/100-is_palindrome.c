fb8561e3c5773f99ec98e09cfa89c7d159991154
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-06 19:47:05 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
+int _palindrome_recursion(char *s, int b, int e);
+int _strlen_recursion(char *s);
+
 /**
  * is_palindrome - a function that returns 1
  * if a string is a palindrome and 0 if not
  * @s: the string
- *
  * Return: an integer
  */
 
 int is_palindrome(char *s)
 {
-        int length = _strlen_recursion(s);
+int e = _strlen_recursion(s);
 
-        return (palindrome_recursion(s, 0, length - 1));
+if (*s == 0)
+{
+return (1);
+}
+return (_palindrome_recursion(s, 0, e - 1));
 }
 
 /**
@@ -23,32 +29,31 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return (0);
-        }
-        return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * palindrome_recursion - is a function helper that checks if the
+ * _palindrome_recursion - is a function helper that checks if the
  * string s is a palindrome
  * @s: a pointer to the string
- * @b: the character in the beginning of the string
- * @e: the character in the end of the string
+ * @b: the beginning of the string
+ * @e: the end of the string
  *
  * Return: an integer
  */
-int palindrome_recursion(char *s, int b, int e)
+int _palindrome_recursion(char *s, int b, int e)
 {
-        if (b >= e)
-        {
-                return (1);
-        }
-        if (s[b] != s[e])
-        {
-                return (0);
-        }
-        return (palindrome_recursion(s, b + 1, e - 1));
+	
+if (b >= e)
+return (1);
+
+if (*(s + b) != *(s + e))
+return (0);
+
+return (_palindrome_recursion(s, b + 1, e - 1));
 }
 
