da18967d88e57206d9ed43e6a25ab1827c0c8f7d
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-09 22:52:25 -0700

    argmul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,27 +4,31 @@
 /**
  * main - entry point
  * Description: a function that multiplies two numbers if the argument
- * count is two number and if the program does not receive two arguments, 
+ * count is two number and if the program does not receive two arguments,
  * it print Error and return 1
  * @argc: arguements count
  * @argv: arguments
  *
- * Return : an integer
+ * Return: an integer
 */
+
 int main(int argc, char *argv[])
 {
 	int i, args;
 	int result = 1;
 
+	if (argc >= 2)
+	{
 	for (i = 1; i < argc; i++)
 	{
 		args = atoi(argv[i]);
 
 		result = result * args;
 	}
+	return (result);
 	printf("%d\n", result);
-
-	if(argc < 2)
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
