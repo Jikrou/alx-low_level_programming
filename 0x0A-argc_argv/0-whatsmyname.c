2efa24f3aae0ba4e106ef1a5eba76480f77e12ff
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-09 19:38:55 -0700

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-/***/
+/**
+ * main - entry point
+ * Description: a function that prints its name,
+ * followed by a new line.
+ * @argc: count of arguments
+ * @argv: the arguments
+ *
+ * Return: an integer
+*/
 int main(int argc, char *argv[])
 {
 	if (argc <= 1)
