200f41141ce434a03364cbaad4290b795011913e
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-11 20:33:53 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/**/
+#include <stdlib.h>
+/**
+ * free_grid - a function that frees a 2 dimensional
+ * grid previously created by your alloc_grid function.
+ * @grid: pointer to an integer
+ * @height: an integer
+*/
 void free_grid(int **grid, int height)
 {
 	int i;
