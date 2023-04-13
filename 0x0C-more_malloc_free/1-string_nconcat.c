4e5608db04c79d3210282fb20c760243d17d68ba
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-13 16:25:09 -0700

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - a function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: a pointer to the newly allocated
+ * space in memory, which contain s1 and the
+ * first n bytes of s2
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len = strlen(s1) + strlen(s2) + 1;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	strcat(str, s1);
+	strncat(str, s2, n);
+	return (str);
+}
