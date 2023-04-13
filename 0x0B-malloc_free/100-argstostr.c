8f9faa62978127afe0a737fb20f0074aa95bb553
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-12 17:54:42 -0700

    args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - a function that concatenates all the arguments
+ * of your program.
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: a pointer character
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+	size_t l;
+	char *ar;
+	int n = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	l = strlen(*av) + 1;
+	ar = malloc((ac + 1) * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		l = 0;
+		while (av[i][l] != '\0')
+		{
+			ar[n] = av[i][l];
+			n++;
+			l++;
+		}
+		ar[n] = '\n';
+		n++;
+	}
+	ar[n] = '\0';
+	return (ar);
+}
