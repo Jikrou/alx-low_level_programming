07f552307dbcec42cc2513584aac221a993ab057
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-13 17:07:41 -0700

    str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,12 +16,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = ("");
 	if (s2 == NULL)
-		s2 = "";
+		s2 = ("");
 
 	len = strlen(s1) + strlen(s2) + 1;
 	str = malloc(len * sizeof(char));
+
 	if (str == NULL)
 		return (NULL);
 
