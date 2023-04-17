9da0e98ce41b44efbb70cb6fb4b8e8d810ab947a
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-17 16:57:30 -0700

    dof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a block of memory
+ * Description: a block of memeory holding the following elements
+ * @name: a string
+ * @age: an integer
+ * @owner: a string
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
