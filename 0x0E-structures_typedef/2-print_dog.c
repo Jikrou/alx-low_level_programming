ecee36cd7d17b6c14cc8f2e4f7c39ebfd8f4789e
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-17 18:27:19 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,12 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+	else if (d != NULL)
 	{
-	printf("Name: %s\n", d->name);
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Name: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
