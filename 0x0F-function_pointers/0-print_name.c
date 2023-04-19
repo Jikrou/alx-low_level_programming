76fad00991841ed16e7654faf826b364c243dd52
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-19 15:12:14 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - a function that prints a name.
  * @name: a string
@@ -6,5 +7,9 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL)
 	f(name);
+
+	if (name == NULL || f == NULL)
+		return;
 }
