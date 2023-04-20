d2b64b2ded4735c4ca89c3042574ba1864e6e7be
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-20 16:39:12 -0700

    string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - a function that prints strings, followed by a new line.
+ * @separator: a character
+ * @n: integer
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(args, char *));
+		if (separator && i < n - 1)
+		printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
