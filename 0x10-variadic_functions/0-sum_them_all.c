2cb18869f9c38678ae34056ea867d5f39082038c
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-20 15:43:18 -0700

    _sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: constant integer
+ * Return: an integer
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list num;
+
+	if (n == 0)
+		return (0);
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num, int);
+	}
+
+	va_end(num);
+
+	return (sum);
+}
+
