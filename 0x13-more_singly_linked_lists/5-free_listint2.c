b4455038958d6d9271954d6e79bdecc0f040877b
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-05-02 08:22:46 -0700

    tsk5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: a pointer to a pointer to the head of the list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *fl;
+
+	while (*head != NULL)
+	{
+		fl = *head;
+		*head = (*head)->next;
+		free(fl);
+	}
+}
