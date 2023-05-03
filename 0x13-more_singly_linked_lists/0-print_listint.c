91ed71d6a10d8246e8cf1c7b8e99f98cd08c2bae
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-05-02 22:30:04 -0700

    tsk0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t ndm = 0;
 
-	if (h == NULL)
-		return (0);
+if (h == NULL)
+return (0);
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		ndm++;
-	}
-	return (ndm);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+ndm++;
+}
+return (ndm);
 }
