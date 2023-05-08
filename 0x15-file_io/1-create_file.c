5cf7719740e2d54464aba9599e494005ec614942
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-05-08 16:04:57 -0700

    write&read pr

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file -  a function that creates a file.
+ * @filename: pointer to the file name
+ * @text_content: pointer to the string in the file
+ * Return: 1 on success and -1 if it fails
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int r;
+	char *b;
+	int len;
+
+	r = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (r == -1)
+	return (-1);
+
+	len = strlen(text_content) + 1;
+	b = malloc(sizeof(char) * (len));
+	if (b == NULL)
+	{
+	close(r);
+	return (-1);
+	}
+
+	strcpy(b, text_content);
+	write(r, text_content, len);
+
+	close(r);
+	free(b);
+	return (1);
+}
