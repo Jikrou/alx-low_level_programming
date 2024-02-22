f885c573d12c844c65c10fd15f13c5042f2e7b93
jikrou <121866583+Jikrou@users.noreply.github.com>
2024-02-22 06:39:34 -0800

    0-binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * binary_to_unit - a function that converts a binary number to
+ * binary_to_uint - a function that converts a binary number to
  * an unsigned int.
  * @b: a pointer to string of 0 and 1 chars
  * Return: the converted number or 0 otherwise
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int r = 0;
 	int i, l;
@@ -21,7 +21,7 @@ unsigned int binary_to_unit(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		r = r << '1';
+		r = r << 1;
 		if (b[i] == '1')
 			r = r | 1;
 	}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sdlib.h>
+#include <stdlib.h>
 #include <string.h>
 
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
