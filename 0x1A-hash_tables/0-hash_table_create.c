6bc09aa5d447c832cc08f0fdf41fa5db0fc23246
Jikrou <jamaleddine.zait@gmail.com>
2024-04-12 21:36:58 +0000

    ht c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 
+	if (size == 0)
+		return (NULL);
+
 	new->array = malloc(size * sizeof(hash_node_t *));
 	if (new->array == NULL)
 	{
