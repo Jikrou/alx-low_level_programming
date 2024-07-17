22f4eca5d68dc1f7d2f59e62eec77400edd19109
Jikrou <jamaleddine.zait@gmail.com>
2024-07-17 20:00:43 +0000

    Jup

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -31,7 +31,7 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%i] and [%i]\n", l, h);
 
 	h = (h < (int) size) ? h : (int) size - 1;
-	for (; l <= h && h <= (int)size; l++)
+	for (; l <= h; l++)
 	{
 		printf("Value checked array[%i] = [%i]\n", l, array[l]);
 		if (value == array[l])
